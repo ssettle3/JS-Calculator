1718fe409f735e3267b69c69b0ae231a8be72f2e
Stephen Settle <ssettle3@gmail.com>
2015-01-14 14:48:20 -0500

    Bonus HW add subtract dvide and multiply complete

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ var input1, input2, button, output, inputVal1, inputVal2;
 input1 = document.getElementById('num_one');
 input2 = document.getElementById('num_two');
 button = document.getElementById('calculateBtn');
+button2 = document.getElementById('calculateBtn2')
+button3 = document.getElementById('calculateBtn3')
+button4 = document.getElementById('calculateBtn4')
 output = document.getElementById('answer');
 
 /*	TO DO ITEMS
@@ -18,9 +21,34 @@ console.log('Input1', input1);
 console.log('Input2', input2);
 console.log('Answer', output);
 
+//Adding Portion
+
 button.onclick = function (){
-	console.log('Button was clicked dumbass');
 	var inputVal1 = input1.value;
 	var inputVal2 = input2.value;
 	output.innerHTML = parseFloat(inputVal1) + parseFloat(inputVal2);
+}
+
+//Subtracting Portion
+
+button2.onclick = function (){
+	var inputVal1 = input1.value;
+	var inputVal2 = input2.value;
+	output.innerHTML = parseFloat(inputVal1) - parseFloat(inputVal2);
+}
+
+//Multiplying Portion
+
+button3.onclick = function (){
+	var inputVal1 = input1.value;
+	var inputVal2 = input2.value;
+	output.innerHTML = parseFloat(inputVal1) * parseFloat(inputVal2);
+}
+
+//Dividing Portion
+
+button4.onclick = function (){
+	var inputVal1 = input1.value;
+	var inputVal2 = input2.value;
+	output.innerHTML = parseFloat(inputVal1) / parseFloat(inputVal2);
 }
